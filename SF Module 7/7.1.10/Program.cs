03ff046bcd341f4849e9853f7bfa82a07b9540e8
Dmitry <ryabchikk@rambler.ru>
2022-10-14 19:38:46 +0300

    7.1.10

---
+++ SF Module 7/7.1.10/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+class BaseClass
+{
+    protected string Name;
+
+    public BaseClass(string name)
+    {
+        Name = name;
+    }
+}
+
+class DerivedClass : BaseClass
+{
+    public string Description;
+
+    public int Counter;
+
+    public DerivedClass(string Name, string Description) : base(Name)
+    {
+        this.Description = Description;
+    }
+
+    public DerivedClass(string Name, string Description, int Counter) : this(Name, Description)
+    {
+        this.Counter = Counter;
+    }
+
+}
