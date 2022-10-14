e3d2b95287bbd1fff8da7eb26f6030f1238e8f9b
Dmitry <ryabchikk@rambler.ru>
2022-10-14 18:54:45 +0300

    7.1.5

---
+++ SF Module 7/7.1.4/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+class Employee
+{
+    public string Name;
+    public int Age;
+    public int Salary;
+}
+
+class ProjectManager : Employee
+{
+    public string ProjectName;
+}
+
+class Developer : Employee
+{
+    public string ProgrammingLanguage;
+}
+
---
+++ SF Module 7/7.1.5/Program.cs
@@ -0,0 +1,44 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+class Apple : Fruit
+{
+
+}
+
+class Banana : Fruit
+{
+
+}
+class Pear : Fruit
+{
+
+}
+class Potato : Vegetable
+{
+
+}
+class Carrot : Vegetable
+{
+
+}
+
+class Fruit : Food
+{
+
+}
+
+class Vegetable : Food
+{
+
+}
+
+class Food
+{
+
+}
