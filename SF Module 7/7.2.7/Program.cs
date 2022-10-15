ed501f8df09720e749208e0d0678485a7c80c85d
Dmitry <ryabchikk@rambler.ru>
2022-10-15 18:14:42 +0300

    7.2.7

---
+++ SF Module 7/7.2.7/Program.cs
@@ -0,0 +1,44 @@
+﻿using static System.Console;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+class A
+{
+    public virtual void Display()
+    {
+        WriteLine("A");
+    }
+}
+class B : A
+{
+    public new void Display()
+    {
+        WriteLine("B");
+    }
+}
+class C : A
+{
+    public override void Display()
+    {
+        WriteLine("C");
+    }
+}
+class D : B
+{
+    public new void Display()
+    {
+        WriteLine("D");
+    }
+}
+class E : C
+{
+    public new void Display()
+    {
+        WriteLine("E");
+    }
+}
