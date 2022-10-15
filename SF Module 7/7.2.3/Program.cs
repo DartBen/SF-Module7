efe4bbed81933d0567f30844b0ba454d17a4e407
Dmitry <ryabchikk@rambler.ru>
2022-10-15 16:55:19 +0300

    7.2.3

---
+++ SF Module 7/7.2.3/Program.cs
@@ -0,0 +1,36 @@
+﻿using static System.Console;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        BaseClass b=new BaseClass();
+        DerivedClass d1=new DerivedClass();
+        DerivedClassSecond d2 = new DerivedClassSecond();
+
+        b.Display();
+        d1.Display();
+        d2.Display();
+
+
+    }
+}
+
+class BaseClass
+{
+    public virtual void Display() { WriteLine("Метод класса BaseClass"); }
+}
+
+class DerivedClass : BaseClass
+{
+    public override void Display() { WriteLine("Метод класса DerivedClass"); }
+}
+
+
+class DerivedClassSecond : DerivedClass
+{
+    public override void Display()
+    {
+        WriteLine("Метод класса DerivedClassSecond");
+    }
+}
+
