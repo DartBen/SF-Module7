52d0df6c841234d6f0fe6db129d812abbf6d74f1
Dmitry <ryabchikk@rambler.ru>
2022-10-15 18:01:28 +0300

    7.2.5

---
+++ SF Module 7/7.2.4/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+class BaseClass
+{
+    public virtual int Counter
+    {
+        get;
+        set;
+    }
+}
+
+class DerivedClass : BaseClass
+{
+    public override int Counter
+    {
+        get { return Counter; }
+        set
+        {
+            if (value < 0)
+            { Console.WriteLine("Значение меньше 0"); }
+            else { Counter = value; }
+        }
+    }
+}
