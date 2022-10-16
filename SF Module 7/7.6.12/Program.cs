8f3bef4cc9f0efa2360bd09f018b853ea6b66574
Dmitry <ryabchikk@rambler.ru>
2022-10-16 18:56:53 +0300

    7.6.12

---
+++ SF Module 7/7.6.12/Program.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+class ElectricEngine : Engine { }
+class GasEngine : Engine { }
+class Battery : CarPart { }
+class Differential : CarPart { }
+class Whee : CarPart { }
+
+abstract class Engine { }
+abstract class CarPart { }
+
+class Car<TEngine> where TEngine : Engine
+{
+    public TEngine Engine;
+    public virtual void ChangePart<TPart>(TPart newPart) where TPart : CarPart
+    {
+    }
+}
+
+class ElectricCar : Car<ElectricEngine>
+{
+    //public override void ChangePart<TPart>(TPart newPart)
+    //{
+    //}
+}
+
+class GasCar : Car<GasEngine>
+{
+    //public override void ChangePart<TPart>(TPart newPart)
+    //{
+    //}
+}
