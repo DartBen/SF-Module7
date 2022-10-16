ef1dc4be23672c0f946c47a1ea232781caba6f72
Dmitry <ryabchikk@rambler.ru>
2022-10-16 17:54:08 +0300

    7.6.7

--- SF Module 7/7.6.6/Program.cs
+++ SF Module 7/7.6.6/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+class Record<T1, T2>
+{
+    public T1 Id;
+    public T2 Value;
+    public DateTime Date;
+}
+
---
+++ SF Module 7/7.6.7/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+class ElectricEngine { }
+class GasEngine { }
+class Battery { }
+class Differential { }
+class Whee { }
+
+class Car<T>
+{
+    public T Engine;
+    public virtual void ChangePart<T2>(T2 newPart)
+    {
+    }
+
+}
+
+
