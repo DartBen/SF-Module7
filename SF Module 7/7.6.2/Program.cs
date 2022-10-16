fbaaf0b6ef497a93b5c6057803383d2e833c4187
Dmitry <ryabchikk@rambler.ru>
2022-10-16 17:29:22 +0300

    7.6.2

---
+++ SF Module 7/7.6.2/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+class ElectricEngine { }
+
+class GasEngine { }
+
+class Car<T>
+{
+    public T Engine;
+}
+
+
