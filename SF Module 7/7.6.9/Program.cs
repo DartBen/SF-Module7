21ce2dc33a1f4589ae79e1efdc99cfc7a6fe17c3
Dmitry <ryabchikk@rambler.ru>
2022-10-16 18:24:31 +0300

    7.6.10

---
+++ SF Module 7/7.6.9/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+class ElectricEngine : Engine { }
+class GasEngine : Engine { }
+class Battery : CarPart { }
+class Differential : CarPart { }
+class Whee : CarPart { }
+
+abstract class Engine { }
+abstract class CarPart { }
+
+class Car<T> where T : Engine
+{
+    public T Engine;
+    public virtual void ChangePart<T2>(T2 newPart) where T2 : CarPart
+    {
+
+    }
+
+}
