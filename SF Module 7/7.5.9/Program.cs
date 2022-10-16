88837b2c68bdd933dd55719c65e87ff78c706822
Dmitry <ryabchikk@rambler.ru>
2022-10-16 17:16:46 +0300

    7.5.9

--- SF Module 7/7.5.9/Program.cs
+++ SF Module 7/7.5.9/Program.cs
@@ -2,7 +2,16 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int num1 = 7;
+        int num2 = -13;
+        int num3 = 0;
+
+        Console.WriteLine(num1.GetNegative()); //-7
+        Console.WriteLine(num1.GetPositive()); //7
+        Console.WriteLine(num2.GetNegative()); //-13
+        Console.WriteLine(num2.GetPositive()); //13
+        Console.WriteLine(num3.GetNegative()); //0
+        Console.WriteLine(num3.GetPositive()); //0
     }
 }
 
@@ -27,9 +36,5 @@ static class intExtension
         else { result = value * (-1); }
 
         return result;
-    }
-
-
-
-    
+    } 
 }
